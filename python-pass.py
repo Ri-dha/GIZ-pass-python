d084a9122213b24ec0b8be792c869a57818ac512
Ri-dha <tareqridha@gmail.com>
2021-10-01 00:27:41 +0300

    sloution for py task

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,33 @@
-
-
 class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        # function to find the longest palindrome from the string
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+        
+        def pointersfun(left,right):
+
+            #we start from the middle of the string then go left and right to find out the longest Palindrome
+
+            while (left >= 0 and right < len(s) and s[left]==s[right]):
+                left-= 1
+                right+= 1
+            return s[left+1:right]
+
+        # we store the result of the longestPalindrome
+        palindrome = ""
+        
+        # for loop for two cases (odd number of characters string and even number of characters string)
+         
+        for i in range(len(s)):
+            # for odd numbers characters 
+            initPalndrome = pointersfun(i,i)
+            if len(initPalndrome) > len(palindrome): palindrome = initPalndrome
+
+            #two pointers for even numbers characters   
+            initPalndrome = pointersfun(i,i+1)
+
+            # check which palindrome is the longest then store it in the result variable
+            if len(initPalndrome) > len(palindrome): palindrome = initPalndrome    
+        print (palindrome) 
+
+    s = input('enter a string:')
+    longestPalindrome('self',s)
